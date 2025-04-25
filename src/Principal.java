a7eb6762d22df8691b3256b32c91585cb0e0e86a
13CrysM <crysmupe@gamil.com>
2025-04-25 15:59:56 -0600

    Crea clases Moneda y ConsultaMoneda

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Conversión de monedas.");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        ConsutaMoneda consulta = new ConsutaMoneda();
+        System.out.println("Ingresa la moneda:  ");
+        try {
+            var denMon = lectura.nextLine();
+            Moneda moneda = consulta.buscaMoneda(denMon);
+            System.out.println(moneda);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        //System.out.println("Conversión de monedas.");
         System.out.println("Adios");
+
+        String direccion = "https://v6.exchangerate-api.com/v6/f2c3223eb6f05839fc291c4e/latest/MXN";
+
+
     }
 }
